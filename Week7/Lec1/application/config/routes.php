852f1036269bcb884b19943ae5e9defdd078a831
thisha-me <d.thishakya@gmail.com>
2026-03-03 13:44:59 +0530

    week7 lec

--- Week7/Lec1/application/config/routes.php
+++ Week7/Lec1/application/config/routes.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -52,4 +52,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['api/user'] = 'api/user/index';
+$route['api/users'] = 'api/user/index';
